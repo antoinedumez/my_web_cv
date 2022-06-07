0f321619d3942ca05d401bbcebea60af1dd75cfe
Antoine Dumez <MY_NAME@example.com>
2022-06-07 15:26:53 +0200

    moustache

--- assets/app.js
+++ assets/app.js
@@ -15,13 +15,13 @@ var typewriter = new Typewriter(app, {
     loop: false
 });
 
-typewriter.typeString('<strong>motivé !</strong>')
+typewriter.typeString('<strong>rigoureux</strong>')
     .pauseFor(2500)
     .deleteAll().changeDeleteSpeed(1)
     .typeString('<strong>motivé !</strong>')
     .pauseFor(2000)
     .deleteAll().changeDeleteSpeed(1000)
-    .typeString('<strong>rigoureux!</strong>')
+    .typeString('<strong>qui a une moustache (ou presque...)</strong>')
     .deleteAll()
     .typeString('<strong>qui aime apprendre !</strong>')
     .pauseFor(2500)
